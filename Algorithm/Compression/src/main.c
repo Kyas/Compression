f3e03f2160a7e8c48d20c13c78682e026b6f4f4f
Jeremy Lor <Jrmy Lor@Jeremy-PC>
2012-04-23 19:02:00 +0200

    Integration of the main of project.

---
+++ Algorithm/Compression/src/main.c
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * 
+ * Copyright 2012 
+ * LE ROUX Thomas <thomas@november-eleven.fr>
+ * LOR Jérémy <jeremyk.lor@gmail.com>
+ * 
+ * This file is part of the Algorithm Project : Compression.
+ * 
+ * Compression is free software: you can redistribute it and/or modify
+ * it under the terms of the zlib license. See the COPYING file.
+ * 
+ *****************************************************************************/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+int main() {
+
+	return 0;
+}
