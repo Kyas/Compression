4754d293f741de8c609c95890a0a0c0584a16d1a
Jeremy Lor <Jeremy@Jeremy-PC.(none)>
2012-05-19 20:35:02 +0200

    Delete Java Project

--- Java/NotesElevesProfesseurs_V1/src/notesElevesProfesseurs/exceptions/EleveInexistant.java
+++
@@ -1,9 +0,0 @@
-package notesElevesProfesseurs.exceptions;
-
-@SuppressWarnings("serial")
-public class EleveInexistant extends Exception {
-
-	public EleveInexistant(String message) {
-		super(message);
-	}
-}
--- Java/NotesElevesProfesseurs_V1/src/notesElevesProfesseurs/exceptions/NotesVides.java
+++
@@ -1,9 +0,0 @@
-package notesElevesProfesseurs.exceptions;
-
-@SuppressWarnings("serial")
-public class NotesVides extends Exception {
-
-	public NotesVides(String message) {
-		super(message);
-	}
-}
